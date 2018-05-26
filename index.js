ce3096b2ec0f8bb83e5e6f115bc28d21bbecf0a9
Shaun Cutts <shaunc@factfiber.com>
2018-05-26 16:18:48 -0400

    revert coffeescript to 1.x for schematist compatibility

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // require coffee if possible; js otherwise
 try {
-  try { require('coffee-script/register'); } catch (e) {}  
+  try { require('coffeescript/register'); } catch (e) {}
   xwrap = require('./src/xwrap');
 }
 catch (e) {
-  if(e.message.indexOf("Cannot find module") != -1 
-      && (e.message.indexOf('./src/index') != -1 
-        || e.message.indexOf('coffee-script/register') != -1))
+  if(e.message.indexOf("Cannot find module") != -1
+      && (e.message.indexOf('./src/index') != -1
+        || e.message.indexOf('coffeescript/register') != -1))
     xwrap = require('./lib/xwrap');
   else
     throw e;
